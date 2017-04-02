690087d2076c1aa7c9593c4970366f8dec411059
abadreldin <amirab37@gmail.com>
2017-04-01 21:52:28 -0700

    update

--- Haptic_Resp_V10_Linear_Controller/findStartPosition.ino
+++ Haptic_Resp_V10_Linear_Controller/findStartPosition.ino
@@ -1,14 +1,17 @@
 void findStartPosition (){
+  static int speedCounter =0;
   
 // code pushes Red motor to one corner, until not reading any change in position (reached boundary), 
 // then pushes blue motor to end until not reading any change in position, then press reset on decoder
 
 //RED MOTOR BOUNDS - X
 
-  setRedMotorSpeed(170, OUTWARD);
-  
-  delay(300);
-  int pastPosition = redActualPosition;
+  while(speedCounter < 250){
+    setRedMotorSpeed(speedCounter, OUTWARD);
+    speedCounter++;
+    delay(5);
+  }
+  /*int pastPosition = redActualPosition;
   Serial.println (pastPosition);
   delay(100);
   int currentPosition = redActualPosition;
@@ -18,18 +21,23 @@ void findStartPosition (){
     pastPosition=currentPosition;
     delay(200);
     currentPosition = redActualPosition;
-  }
+  }*/
   delay(500);
   digitalWrite(redResetPin,LOW);
   delay(10);
   digitalWrite(redResetPin,HIGH);
   digitalWrite(redLatchPin, HIGH); 
   setRedMotorSpeed(0, OFF);
-  Serial.println ("FINISHED INITIALIZATION");
+  //Serial.println ("FINISHED INITIALIZATION");
 
 //BLUE MOTOR BOUNDS - Y
-
-  setBlueMotorSpeed(170, OUTWARD);
+  speedCounter = 0;
+  while(speedCounter < 250){
+    setBlueMotorSpeed(speedCounter, OUTWARD);
+    speedCounter++;
+    delay(5);
+  }
+  /*setBlueMotorSpeed(200, OUTWARD);
   pastPosition = blueActualPosition;
   delay(300);
   currentPosition = blueActualPosition;
@@ -38,7 +46,7 @@ void findStartPosition (){
     pastPosition=currentPosition;
     delay(200);
     currentPosition = blueActualPosition;
-  }
+  }*/
   delay(500);
   setBlueMotorSpeed(0, OFF);
   digitalWrite(blueResetPin,LOW);
